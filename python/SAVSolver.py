d19fc698de9c3cbf1abdd274d7d3ba1ace91383c
thomas-risse <risse@ircam.fr>
2025-11-24 13:39:53 +0100

    Added comments to the solver

--- python/SAVSolver.py
+++ python/SAVSolver.py
@@ -4,7 +4,7 @@ from time import sleep
 from Model import Model
 
 class SAVSolver():
-    """General SAV solver using same matrices thatn in the JAES paper.
+    """General SAV solver using same notations than in the JAES paper.
     """
     def __init__(self,model, sr = 44100):
         ### System definition ###
@@ -37,6 +37,9 @@ class SAVSolver():
         self.check_sizes()
 
     def check_sizes(self):
+        """Checks that the underlying model has coherent matrices and operators
+        shapes.
+        """
         # Check that all matrices and operator provided to describe the 
         # system are of the right dimensions
         x = np.zeros(self.model.N)
@@ -83,14 +86,102 @@ class SAVSolver():
         if (Fnl_val.shape[0] != self.model.N or self.model.J0.ndim != 1):
             raise Exception(f"Fnl(q) evaluated using EandFnl(q) must have {self.model.N} elements but has shape {Fnl_val.shape}")
         
+    def Ek_tilde(self, p):
+        """Returns 0.5 * p^T Mtilde^{-1} p, correponding to the 
+        kinetic part of the pseudo-energy preserved by the scheme.
+
+        Args:
+            p (vector): generalized momentum vector
+
+        Returns:
+            number: kinetic part of the pseudo-energy
+        """
+        return 0.5 * p.dot(1 / self.model.M * p 
+                            - self.dt2 / 4 * self.model.J0 / self.model.M * self.model.K_op(self.model.J0 / self.model.M * p) 
+                            - self.dt / (2 * self.model.M) * self.model.Rsv_op(p/self.model.M))
+    
+    def Ek_tilde(self, p):
+        """Returns 0.5 * p^T M^{-1} p, correponding to the 
+        kinetic part of the energy of the conitnuous time system.
+
+        Args:
+            p (vector): generalized momentum vector
+
+        Returns:
+            number: kinetic part of the continuous time energy
+        """
+        return 0.5 * p.dot(1 / self.model.M * p)
+    
+    def Ep_lin(self, q):
+        """Returns 0.5 * q^T  K  q, correponding to the linear
+        part of the potential energy.
+
+        Args:
+            q (vector): generalized coordinate vector
+
+        Returns:
+            number: linear part of the potential energy
+        """
+        return 0.5 * q.dot(self.model.K_op(q))
+
+    def Ep_nl(self, q):
+        """Returns the nonlinear part of the potential energy
+        evaluated using the q corrdinates
+
+        Args:
+            q (vector): generalized coordinate vector
+
+        Returns:
+            number: nonlinear part of the potential energy evaluated from q
+        """
+        return self.model.Enl(q)
+
+    def Ep_nl_sav(self, r):
+        """Returns the nonlinear part of the potential energy
+        evaluated using the scalar auxiliary variable
+
+        Args:
+            r (number): scalar auxiliary variable
+
+        Returns:
+            number: nonlinear part of the potential energy evaluated from r
+        """
+        return 0.5 * r * r
 
     def A0_inv(self, Rmid):
+        """Recomputes A0_inv used in Shermann-Morrison solving 
+        (eq 19h) from the expression of Rmid.
+
+        Args:
+            Rmid (vector): diagonal dissipation matrix Rmid
+
+        Returns:
+            vector: diagonal matrix A0_inv
+        """
         return 2 * self.dt2 * np.ones(self.model.N) / (2 * self.model.M + self.dt * Rmid)
     
     def B_op(self, q):
+        """Applies the B operator to the vector q^{n+1/2} (eq 19b, 19e)
+
+        Args:
+            q (vector): generalized coordinates vector
+
+        Returns:
+            vector: B q
+        """
         return 2 * self.model.M / (self.dt2 * self.model.J0) * q - self.model.J0 * self.model.K_op(q) - self.model.Rsv_op(q / self.model.J0) / self.dt
     
     def C_op(self, q, g, Rmid):
+        """Applies the C operator to the vector q^{n-1/2} (eq 19b, 19f)
+
+        Args:
+            q (vector): generalized coordinates vector
+            g (vector): g vector (eq 19a)
+            Rmid (vector): diagonal dissipation matrix Rmid
+
+        Returns:
+            vector: C q
+        """
         return - np.ones(self.model.N) * \
             (
                 (self.model.M / self.dt2
@@ -101,14 +192,47 @@ class SAVSolver():
             )
                                               
     def g(self, q):
+        """Return the g vector computed from q as (eq 9b)
+        J0 fnl(q) / sqrt(2 * Enl(q) + C0)
+
+        Args:
+            q (vector): generalized coordinates vector
+
+        Returns:
+            vector: g_std(q)
+        """
         Enl, Fnl = self.model.EandFnl(q)
         return self.model.J0 * Fnl / (np.sqrt(2 * Enl + self.C0 + self.model.Num_eps))
     
     def g_mod(self, q, p, r):
+        """Returns the modification of g term to include 
+        the drift controller
+
+        Args:
+            q (vector): generalized coordinates vector
+            p (vector): momentum vector
+            r (number): scalar auxiliary variable
+
+        Returns:
+            vector: g_mod(q,p, r)
+        """
         self.epsilon = r - np.sqrt(2 * self.model.Enl(q) + self.C0)
         return - self.lamba0 * self.epsilon * self.model.M * np.sign(p) / (np.abs(p) + self.model.Num_eps)
 
     def time_step(self, qlast, qnow, rn, unow, ConstantRmid = False):
+        """Returns next state by solving (eq 19a, 19b, 19c)
+
+        Args:
+            qlast (vector): generalized coordinates (q^{n-1/2})
+            qnow (vector): generalized coordinates (q^{n+1/2})
+            rn (_type_): scalar auxiliary variable (r^n)
+            unow (vector): input
+            ConstantRmid (bool, optional): Specifies if Rmid needs to be recomputed,
+                which corresponds to cases where it is state dependent. Defaults to False.
+
+        Returns:
+            (vector, number): next state (q^{n+3/2}, r^{n+1})
+        """
         # qlast correponds to q^{n-1/2} and qnow to
         # q^{n+1/2}. rn corresponds to r^n. unow to u^{n+1/2}.
         # First, compute g
@@ -135,6 +259,17 @@ class SAVSolver():
         return qnext, rnext
     
     def integrate(self, q0, u0, u_func, duration, plot = None, ConstantRmid = False):
+        """Integrates the dynamics using provided initial conditions, input function, 
+        and duration.
+
+        Args:
+            q0 (vector): Initial condition, generalized coordinates
+            u0 (vector): Initial condition, velocity u_0 = \dot q(t=0)
+            u_func (function): input vector as a function of time
+            duration (number): simulation duration
+            plot (int, optional): Sepcifies which data to plot. Defaults to None.
+            ConstantRmid (bool, optional): Specifies if Rmid needs to be recomputed at each time step. Defaults to False.
+        """
         ### Time vector and storage initialization ###
         self.model.Nt = int(duration / self.dt)
         self.t = np.arange(self.model.Nt) * self.dt
