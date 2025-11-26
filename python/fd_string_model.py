Fixed gmod computation

python/fd_string_model.py
@@ -196,16 +196,15 @@ class FD_string_model(Model):
 
 if __name__ == "__main__":
     sr = 44100
-    string_params = {"NL_type": "linear"}
-    model = FD_string_model(sr, NL_type = "linear")
+    model = FD_string_model(sr, NL_type = "geom")
     print("N=", model.N)
     model.print_perceptual_params()
-    solver = SAVSolver(model, sr = sr)
+    solver = SAVSolver(model, sr = sr, lambda0=1000)
     solver.check_sizes()
     x = np.linspace(0, 1, model.N+2)
-    q0 = np.sin(np.pi*x)[1:-1] * 1
+    q0 = np.sin(np.pi*x)[1:-1] * 1e-2
     u0 = np.zeros(model.N)
     def u_func(t):
         return np.zeros(model.Nu)
-    solver.integrate(q0, u0, u_func, duration = 1, ConstantRmid=True, plotter_config=NO_PLOTTER_CONFIG, storage_config=STATE_STORAGE_CONFIG)
+    solver.integrate(q0, u0, u_func, duration = 1, ConstantRmid=True, plotter_config={"q_idx": np.array([model.N//2]), "p_idx": np.array([model.N//2])}, storage_config={})
     solver.storage.write("results/test_string.h5")

python/sav_solver.py
@@ -22,7 +22,7 @@ class SAVSolver():
         # Control parameter
         self.lambda0 = lambda0
         # Numerical epsilon
-        self.Num_eps = 1e-16
+        self.Num_eps = 1e-14
 
         ### Some vector initialization ###
         self.Gn = np.zeros(self.model.N)
@@ -336,7 +336,7 @@ class SAVSolver():
             vector: g_mod(q,p, r)
         """
         self.epsilon = r - np.sqrt(2 * self.model.Enl(q) + self.C0)
-        return - self.lambda0 * self.epsilon * self.model.M * np.sign(p) / (np.abs(p) + self.Num_eps)
+        return - self.lambda0 * self.epsilon * self.model.M * np.sign(p) / (np.sum(np.abs(p)) + self.Num_eps)
 
     ### Time stepping and integration ###
 